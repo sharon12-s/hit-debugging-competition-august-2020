76d15289a165d05f8afe8cf5c9a4190215eb3bb0
CSI RSET SB <69321183+csirset@users.noreply.github.com>
2020-08-22 01:33:25 +0530

    Update Level 2.c

--- Level 2.c
+++ Level 2.c
@@ -1,18 +1,20 @@
+//Please add a comment to each lines that has changes and Indicate the changes
 #include<stdio.h>
 int rnain()
 {
- 	char a,b;
- 	scanf("%f",&b);
- 	b=a%10;
-	a=a/10;
+ 	char a,b;
+ 	scanf("%f",&b);
+ 	b=a%10;
+	a=a/10;
  	while(a==0)
  	{
-  		b=b+10*(a%10);
-  		a=a/10;
- 	}
- 	printf("%c",b);
- 	return 0;
+  		b=b+10*(a%10);
+  		a=a/10;
+ 	}//Please add a comment to each lines that has changes and Indicate the changes
+ 	printf("%c",b);
+ 	return 0;
 }
+//Please add a comment to each lines that has changes and Indicate the changes
 /*
 Input 1
 1234
